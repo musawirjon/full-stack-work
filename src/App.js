fa87e54f6629a81138187d59479b3b9f3b094ac6
Musawir <musawir@innovastore-international.com>
2022-10-17 19:25:59 +0500

    part1-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+ 
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  }); 
 
-function App() {
+  const [allClicks, setAllClicks] = useState([])
+
+  let increaseByOne = () => setCounter(counter +1)
+
+  const decreaseByOne = () => setCounter(counter - 1)
+
+  let setToZero = () => setCounter(0)
+
+  let setLeftState = () => {
+    setAllClicksState("L"); 
+    setClicks({...clicks, left: clicks.left + 1});
+  };
+
+  let setRightState = () => { 
+    setAllClicksState("R"); 
+    setClicks({...clicks, right: clicks.right + 1});
+  }
+
+  let setAllClicksState = (event) => {
+    setAllClicks(allClicks.concat(event)); 
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Display counter={counter}/>
+      <ClickHistory allClicks={allClicks} />
+      <Button onClick={increaseByOne} text="+Plus" />
+      <Button onClick={decreaseByOne} text="-Minus" />
+      <Button onClick={setToZero} text="Rest" />
+      <Button onClick={setLeftState} text="left" />
+      <Button onClick={setRightState} text="Right" />
     </div>
-  );
+  )
+}
+const Display = ({counter}) => <div>{counter}</div>
+
+const ClickHistory = ({allClicks}) => {
+  if(allClicks.length === 0){
+    return (
+      <div>The app is used by presssing the buttons</div>
+    )
+  }
+  return (
+    <div>
+      Button Press history: {allClicks.join(' ')}
+    </div>
+  )
 }
 
-export default App;
+const Button = ({onClick,text}) => <button onClick={onClick}>{text}</button>
+
+export default App
